8728738b9efb0d0b72ae4f551ad00b7f40c28bfa
kaubu <29447633+kaubu@users.noreply.github.com>
2023-09-09 03:29:10 +0000

    Work on stuff

---
+++ src/components/functions.ts
@@ -0,0 +1,262 @@
+import { Tier } from "./models";
+
+type TierReturn = {
+  name: string,
+  tier: string,
+  ratio: number,
+}
+
+function getTierName(tier: Tier): TierReturn {
+  switch (tier) {
+    case Tier.BELOW_AVERAGE_HUMAN: return {
+      name: "Below Average Human",
+      tier: "10-C",
+      ratio: 1,
+    }
+    case Tier.HUMAN: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.ATHLETE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.STREET: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.WALL: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.SMALL_BUILDING: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.BUILDING: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LARGE_BUILDING: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.CITY_BLOCK: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.MULTI_CITY_BLOCK: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.SMALL_TOWN: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.TOWN: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LARGE_TOWN: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.SMALL_CITY: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.CITY: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.MOUNTAIN: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LARGE_MOUNTAIN: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.ISLAND: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LARGE_ISLAND: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.SMALL_COUNTRY: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.COUNTRY: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LARGE_COUNTRY: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.CONTINENT: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.MULTI_CONTINENT: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.MOON: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.SMALL_PLANET: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.PLANET: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LARGE_PLANET: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.DWARF_STAR: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.SMALL_STAR: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.STAR: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LARGE_STAR: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.SOLAR_SYSTEM: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.MULTI_SOLAR_SYSTEM: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.GALAXY: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.MULTI_GALAXY: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.UNIVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.HIGH_UNIVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.UNIVERSE_PLUS: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LOW_MULTIVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.MULTIVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.MULTIVERSE_PLUS: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LOW_COMPLEX_MULTIVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.COMPLEX_MULTIVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.HIGH_COMPLEX_MULTIVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.HYPERVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.HIGH_HYPERVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.LOW_OUTERVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.OUTERVERSE: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+    case Tier.BOUNDLESS: return {
+      name: "",
+      tier: "",
+      ratio: 1,
+    }
+  }
+}
--- src/components/models.ts
+++ src/components/models.ts
@@ -1,8 +1,102 @@
-export interface Todo {
-  id: number;
-  content: string;
+export enum Tier {
+  BELOW_AVERAGE_HUMAN,      // 10-C
+  HUMAN,                    // 10-B
+  ATHLETE,                  // 10-A
+
+  STREET,                   // 9-C
+  WALL,                     // 9-B
+  SMALL_BUILDING,           // 9-A
+
+  BUILDING,                 // 8-C
+  LARGE_BUILDING,           // High 8-C
+  CITY_BLOCK,               // 8-B
+  MULTI_CITY_BLOCK,         // 8-A
+
+  SMALL_TOWN,               // Low 7-C
+  TOWN,                     // 7-C
+  LARGE_TOWN,               // High 7-C
+  SMALL_CITY,               // Low 7-B
+  CITY,                     // 7-B
+  MOUNTAIN,                 // 7-A
+  LARGE_MOUNTAIN,           // High 7-A
+
+  ISLAND,                   // 6-C
+  LARGE_ISLAND,             // High 6-C
+  SMALL_COUNTRY,            // Low 6-B
+  COUNTRY,                  // 6-B
+  LARGE_COUNTRY,            // High 6-B
+  CONTINENT,                // 6-A
+  MULTI_CONTINENT,          // High 6-A
+
+  MOON,                     // 5-C
+  SMALL_PLANET,             // Low 5-B
+  PLANET,                   // 5-B
+  LARGE_PLANET,             // 5-A
+  DWARF_STAR,               // High 5-A
+
+  SMALL_STAR,               // Low 4-C
+  STAR,                     // 4-C
+  LARGE_STAR,               // High 4-C
+  SOLAR_SYSTEM,             // 4-B
+  MULTI_SOLAR_SYSTEM,       // 4-A
+
+  GALAXY,                   // 3-C
+  MULTI_GALAXY,             // 3-B
+  UNIVERSE,                 // 3-A
+  HIGH_UNIVERSE,            // High 3-A
+
+  UNIVERSE_PLUS,            // Low 2-C
+  LOW_MULTIVERSE,           // 2-C
+  MULTIVERSE,               // 2-B
+  MULTIVERSE_PLUS,          // 2-A
+
+  LOW_COMPLEX_MULTIVERSE,   // Low 1-C
+  COMPLEX_MULTIVERSE,       // 1-C
+  HIGH_COMPLEX_MULTIVERSE,  // High 1-C
+  HYPERVERSE,               // 1-B
+  HIGH_HYPERVERSE,          // High 1-B
+  LOW_OUTERVERSE,           // Low 1-A
+  OUTERVERSE,               // 1-A
+
+  BOUNDLESS,                // 0
+};
+
+enum GenderTypes {
+  MALE,
+  FEMALE,
+  NON_BINARY,
+  INAPPLICABLE,
+  OTHER,
+};
+
+type Gender =
+  | GenderTypes
+  | {"gender": GenderTypes.OTHER, "type": string};
+
+const PowerAbilityList = {
+  "Absolute Zero"
 }
 
-export interface Meta {
-  totalCount: number;
-}
+type PAR = PowerAbility | Resistance;
+
+/**
+ * PAR – Powers, Abilities, and Resistances
+ */
+type PARTabs = Array<PAR>;
+
+interface Key {
+  keyName: string,
+  tier: Tier,
+  characterName: string,
+  age: number,
+  gender: Gender,
+  classification: string,
+  powers_and_abilities: PARTabs,
+};
+
+interface Character {
+  entryName: string,
+  summary: string,
+  origin: string,
+  keys: Array<Key>,
+};
